d34a35efe03f2afbdbed3a5a5761a9182c06f2d7
LuisFRCampos <113317031+LuisFRCampos@users.noreply.github.com>
2022-09-11 21:37:47 -0300

    Create calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,33 @@
+#INCLUDE <STDIO.H>
+INT MAIN (VOID){
+  FLOAT X=0, Y=0, resultado=0;
+  INT OP=0;
+  DO{
+  PRINTF("\n\t1 - SOMA\n\t2 - SUBTRAÇÂO\n\t3 - MULTIPLICAÇÃO\n\t4 - DIVISÃO")
+  SCANF("%I", &OP);
+  PRINTF("\nDigite o primeiro numero: ");
+  scanf("%f", &x);
+  printf("\nDigite o segundo numero: ");
+  scanf("%f", &y);
+  switch(op){
+    case 1:
+      resultado = x+y;
+      break;
+    case 2:
+      resultado = x-y;
+      break;
+    case 3:
+      resultado = x*y
+      break
+    case 4:
+      resultado = x/y
+      break;
+    default
+      printf("\n Digite uma opção validade\n");
+      break;
+  }
+  printf("\n\t o resultado e: %0.2f", resultado);
+  printf("\n Digite 1 para continuar: ");
+  scanf("%i", &op);
+  WHILE(op==1);
+  }
